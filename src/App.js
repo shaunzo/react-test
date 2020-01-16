6556b4c7f0661fa159aa9ec0594549bf55624167
Shaun Reddiar <shaunreddiar@Shauns-iMac.local>
2020-01-16 10:20:54 +0200

    Added props for data table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import DataTable from './components/data-table';
 
 function App() {
- return (
-   <div className="App">
-     <DataTable />
+
+ const tableHeaders = ['Name', 'Email', 'Age', 'Years of experience', 'Position applied', 'Date of application', 'Status of application'];
+ let tableCells = ['-', '-', '-', '-', '-', '-', '-'];
+  return (
+   <div className="App">   
+     <DataTable headers={tableHeaders} cells={tableCells} />
    </div>
  );
 }
--- src/components/data-table.js
+++ src/components/data-table.js
@@ -1,39 +1,30 @@
-import React from 'react';
-
 class DataTable extends React.Component {
-   constructor(props) {
-       super(props);
-       this.state = {}
-   }
-
-   render() {
-       return (
-           <div>
-               <table>
-                   <thead>
-                       <th>Name</th>
-                       <th>Email</th>
-                       <th>Age</th>
-                       <th>Years of experience</th>
-                       <th>Position applied</th>
-                       <th>Date of application</th>
-                       <th>Status of application</th>
-                   </thead>
-
-                   <tbody>
-                       <td>{this.props.children}</td>
-                       <td> - </td>
-                       <td> - </td>
-                       <td> - </td>
-                       <td> - </td>
-                       <td> - </td>
-                       <td> - </td>
-                       <td> - </td>
-                   </tbody>
-               </table>
-               </div>
-       )
-   }
-}
-
-export default DataTable;
+    constructor(props) {
+        super(props);
+    }
+ 
+    render() {
+        return (
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            {this.props.headers.map((header, i) => {
+                                return (<th key={i}>{header}</th>)
+                            })}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            {this.props.cells.map((cell, i) => {
+                                return (<td key={i}>{cell}</td>)
+                             })}
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+ }
+ 
+ export default DataTable;
